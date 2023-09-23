565dcaa46c88e33de6442854ee4bb7a3f50ac6f4
Victor Prudente <victorprudt@gmail.com>
2023-09-23 19:12:46 -0300

    Implementation of new contacts

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,80 @@
 package application;
 
+import entities.Contact;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Agenda");
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        boolean agenda = true;
+        File contacts = new File("contacts.txt");
+
+        while (agenda) {
+            try {
+                System.out.println("Agenda Menu: ");
+                System.out.println("Options: ");
+                System.out.println("1. Show all contacts.");
+                System.out.println("2. Add new contact.");
+                System.out.println("3. Update a contact.");
+                System.out.println("4. Delete a contact.");
+                System.out.println("5. Exit program.");
+                System.out.print("Enter option: ");
+                int option = sc.nextInt();
+                sc.nextLine();
+                switch (option) {
+                    case 1:
+                        System.out.println("All contacts");
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                        break;
+                    case 2:
+                        System.out.println("NEW CONTACT: ");
+                        System.out.print("Name: ");
+                        String name = sc.nextLine();
+                        System.out.print("Phone number: ");
+                        long phoneNumber = sc.nextLong();
+                        System.out.print("Birthday: ");
+                        Date birthDay = sdf.parse(sc.next());
+
+                        Contact contact = new Contact(name, phoneNumber, birthDay);
+                        try (BufferedWriter bw = new BufferedWriter(new FileWriter(contacts))) {
+                            bw.write(contact.toString());
+                            System.out.println("Contact '" + name + "' created sucessfully");
+                        } catch (IOException e) {
+                            System.out.println("Error writing file: " + e.getMessage());
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Update a contact.");
+                        break;
+                    case 4:
+                        System.out.println("Delete a contact.");
+                        break;
+                    case 5:
+                        System.out.println("Exiting agenda.");
+                        agenda = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                        break;
+                }
+            } catch (RuntimeException e) {
+                System.out.println("Unextected error." + e.getMessage());
+            } catch (ParseException e) {
+                System.out.println("Invalid format: " + e.getMessage());
+            }
+        }
     }
 }
--- src/entities/Contact.java
+++ src/entities/Contact.java
@@ -1,23 +1,27 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Contact {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     private String name;
     private Long phoneNumber;
     private Date birthDate;
-    private Date creationDate;
+
 
     public Contact(String name, Long phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
 
-    public Contact(String name, Long phoneNumber, Date birthDate, Date creationDate) {
+    public Contact(String name, Long phoneNumber, Date birthDate) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
-        this.creationDate = creationDate;
     }
 
     public String getName() {
@@ -44,11 +48,16 @@ public class Contact {
         this.birthDate = birthDate;
     }
 
-    public Date getCreationDate() {
-        return creationDate;
-    }
 
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Name: " + name + "\n");
+        sb.append("Phone Number: " + phoneNumber + "\n");
+        sb.append("Birthday: " + sdf.format(birthDate) + "\n");
+
+        return sb.toString();
     }
 }
